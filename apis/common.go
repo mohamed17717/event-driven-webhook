10f67110ff32a03ff558e1fa7dc1eaa308711d9f
Mhmd <d3v.mhmd@gmail.com>
2024-10-05 14:14:09 +0300

    API: - predefined methods to write less code later - method for protect routers

--- apis/common.go
+++ apis/common.go
@@ -1,1 +1,14 @@
 package apis
+
+import (
+	"event-driven-webhook/config"
+	"event-driven-webhook/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func ProtectedRoute() gin.RouterGroup {
+	var route = config.Server.Group("/")
+	route.Use(middlewares.IsAuthenticated())
+
+	return *route
+}
