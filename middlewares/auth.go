a8c37f353a41eaf54f3947a9fcaf1fa5d58d3d68
Mhmd <d3v.mhmd@gmail.com>
2024-10-05 12:32:34 +0300

    Middleware: - create middleware that handle IsAuthenticated and add user to the request

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,1 +1,76 @@
 package middlewares
+
+import (
+	"event-driven-webhook/config"
+	"event-driven-webhook/models"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func IsAuthenticated() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		fmt.Println(tokenString)
+
+		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer ") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token not provided"})
+			c.Abort()
+			return
+		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, http.ErrAbortHandler
+			}
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization token"})
+			c.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			c.Abort()
+			return
+		}
+
+		// Check for token expiration
+		if exp, ok := claims["exp"].(float64); ok {
+			if time.Now().Unix() > int64(exp) {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Token has expired"})
+				c.Abort()
+				return
+			}
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token expiration not found"})
+			c.Abort()
+			return
+		}
+
+		userID := uint(claims["user_id"].(float64))
+
+		var user models.User
+		if err := config.DB.First(&user, userID).Error; err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+			c.Abort()
+			return
+		}
+
+		// Attach user to the context
+		c.Set("user", user)
+		c.Next()
+	}
+}
