e736c0f3ae9a986db8f50c4087b948a89a19bf8e
Mhmd <d3v.mhmd@gmail.com>
2024-10-05 23:50:54 +0300

    Subscriober Action Modle: - some code for bulk create by action and by subscriber to use it later

--- models/subscriber_action.go
+++ models/subscriber_action.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type SubscriberAction struct {
 	ID           uint      `gorm:"primaryKey"`
@@ -8,4 +11,42 @@ type SubscriberAction struct {
 	ActionID     uint      `gorm:"index"`
 	CreatedAt    time.Time `gorm:"default:current_timestamp"`
 	UpdatedAt    time.Time `gorm:"default:current_timestamp"`
+}
+
+func CreateBulkActionsForOneSubscriber(db *gorm.DB, subscriberID uint, actionIDs []uint) error {
+	var subscriberActions []SubscriberAction
+
+	// Create a slice of SubscriberAction
+	for _, actionID := range actionIDs {
+		subscriberActions = append(subscriberActions, SubscriberAction{
+			SubscriberID: subscriberID,
+			ActionID:     actionID,
+		})
+	}
+
+	// Perform the bulk insert
+	if err := db.Create(&subscriberActions).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CreateBulkSubscribersForOneAction(db *gorm.DB, actionID uint, subscriberIDs []uint) error {
+	var subscriberActions []SubscriberAction
+
+	// Create a slice of SubscriberAction
+	for _, subscriberID := range subscriberIDs {
+		subscriberActions = append(subscriberActions, SubscriberAction{
+			SubscriberID: subscriberID,
+			ActionID:     actionID,
+		})
+	}
+
+	// Perform the bulk insert
+	if err := db.Create(&subscriberActions).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
