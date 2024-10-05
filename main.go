5bd34ecf9e3ffaf2290b3c6ef872a31fec105833
Mhmd <d3v.mhmd@gmail.com>
2024-10-05 07:26:13 +0300

    Main: - on start load dotenv , connect db and auto migrate

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"event-driven-webhook/config"
+	"event-driven-webhook/models"
+	"event-driven-webhook/utils"
+)
+
 func main() {
-	loadEnv()
-	connectToDB()
+	utils.LoadEnv()
+	config.ConnectDatabase()
+	err := config.DB.AutoMigrate(
+		&models.User{}, &models.UserConfiguration{},
+		&models.Action{}, &models.Subscriber{}, &models.SubscriberAction{},
+		&models.Change{}, &models.WebhookLog{},
+	)
+
+	utils.CheckErr(err, true)
 }
