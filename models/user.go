5e17784803507597dcc9dcffbe455e0e1b5df4aa
Mhmd <d3v.mhmd@gmail.com>
2024-10-05 12:29:32 +0300

    User Model: - remove field of auth_key - methods to create password and check password

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"time"
 )
 
@@ -11,7 +12,21 @@ type User struct {
 	Password  string    `gorm:"size:255"`
 	FirstName string    `gorm:"size:50"`
 	LastName  string    `gorm:"size:50"`
-	AuthKey   string    `gorm:"size:512;unique"`
 	CreatedAt time.Time `gorm:"default:current_timestamp"`
 	UpdatedAt time.Time `gorm:"default:current_timestamp"`
+}
+
+// Method to hash password
+func (user *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+
+// Method to compare password
+func (user *User) CheckPassword(providedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
 }
