32aa039e024ecc9955013b40faffe3f4484b7948
Mhmd <d3v.mhmd@gmail.com>
2024-10-06 21:08:55 +0300

    Consumers: - consume changes and get all subscriber and poscast tasks for them - consume subscriber tasks get link and send data to the link - register to the main

--- models/subscriber.go
+++ models/subscriber.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"event-driven-webhook/config"
+	"time"
+)
 
 type Subscriber struct {
 	ID          uint      `gorm:"primaryKey"`
@@ -11,4 +14,13 @@ type Subscriber struct {
 	IsActive    bool      `gorm:"default:true"`
 	CreatedAt   time.Time `gorm:"default:current_timestamp"`
 	UpdatedAt   time.Time `gorm:"default:current_timestamp"`
+}
+
+func GetSubscribersForAction(actionID any) []Subscriber {
+	var subscribers []Subscriber
+	config.DB.Joins("JOIN subscriber_actions ON subscribers.id = subscriber_actions.subscriber_id").
+		Where("subscriber_actions.action_id = ?", actionID).
+		Find(&subscribers)
+
+	return subscribers
 }
