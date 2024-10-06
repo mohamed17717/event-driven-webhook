c63041e759a131ed2cab0dc65144d859faa42332
Mhmd <d3v.mhmd@gmail.com>
2024-10-06 20:06:12 +0300

    Changes Endpoint: - set latest change into the redis cache

--- apis/change.go
+++ apis/change.go
@@ -1,12 +1,14 @@
 package apis
 
 import (
+	"bytes"
 	"encoding/json"
 	"event-driven-webhook/config"
 	"event-driven-webhook/models"
 	"event-driven-webhook/viewsets"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"time"
 )
 
@@ -14,7 +16,7 @@ type CreateChangeInput struct {
 	ActionID   uint      `json:"action_id" binding:"required"`
 	Data       string    `form:"data" json:"data" binding:"required"`
 	Identifier string    `form:"identifier" json:"identifier" binding:"required"`
-	When       time.Time `form:"when" json:"when" binding:"required"`
+	When       time.Time `form:"when" json:"when" binding:"required" time_format:"2006-01-02T15:04:05Z07:00"`
 }
 
 func CustomChangeCreate(c *gin.Context, obj *models.Change) error {
@@ -32,6 +34,19 @@ func InputToChange(data *CreateChangeInput) models.Change {
 	}
 }
 
+func readBody(c *gin.Context) ([]byte, error) {
+	// Read the body and store it
+	bodyBytes, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Restore the io.ReadCloser to the original state so it can be read again
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	return bodyBytes, nil
+}
+
 func ChangeRoutes() {
 	protected := ProtectedRoute()
 
@@ -42,11 +57,14 @@ func ChangeRoutes() {
 	}
 
 	creatChange := func(c *gin.Context) {
+		//take a copy of the request
+		bodyBytes, err := readBody(c)
 		//just normal create
 		changeViewSet.Create(c)
-		// Publish change to rabbit mq
+
+		// load the data
 		var data CreateChangeInput
-		c.ShouldBindJSON(&data)
+		json.Unmarshal(bodyBytes, &data)
 
 		bytesData, err := json.Marshal(data)
 		if err != nil {
@@ -54,7 +72,11 @@ func ChangeRoutes() {
 			return
 		}
 
+		// Publish change to rabbit mq
 		config.MQPublishChange(string(bytesData))
+
+		changeTime, _ := data.When.MarshalText()
+		config.RedisSet(data.Identifier, string(changeTime))
 	}
 
 	protected.POST("/changes", creatChange)
